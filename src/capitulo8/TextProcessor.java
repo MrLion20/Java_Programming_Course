50f0e5be6083c167b24f37f6fb0d34e20064b37d
MrLion20 <jus20leon@gmail.com>
2024-11-11 21:18:20 -0600

    Excercise for chapter 8 - Incluid a Method to modifiedText in a String

--- src/capitulo8/TextProcessor.java
+++ src/capitulo8/TextProcessor.java
@@ -6,6 +6,7 @@ public class TextProcessor {
     public static void main (String[] args){
         countWords("I love Test Automation University");
         reverseString("Hello TAU!");
+        addSpaces("HelloWord!It'sMeJustin");
     }
 
     /**
@@ -23,11 +24,20 @@ public class TextProcessor {
         for (String word : words) {
             System.out.println(word);
         }
-
     }
     public static void reverseString(String text){
         for (int i=text.length()-1; i>=0; i--){
             System.out.print(text.charAt(i));
         }
+    }
+    public static void addSpaces(String text){
+        var modifiedText = new StringBuilder(text);
+        for (int i=0; i<modifiedText.length(); i++){
+            if (i!=0 && Character.isUpperCase(modifiedText.charAt(i))){
+                modifiedText.insert(i, " ");
+                i++;
+            }
+        }
+        System.out.print(modifiedText);
     }
 }
