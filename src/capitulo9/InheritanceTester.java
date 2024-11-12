48c60446973ca746f1506a687c2ad0eaf985ca92
MrLion20 <jus20leon@gmail.com>
2024-11-12 13:19:49 -0600

    Incluid excercise of overriding methods

--- src/capitulo9/InheritanceTester.java
+++ src/capitulo9/InheritanceTester.java
@@ -1,8 +1,15 @@
 package capitulo9;
 
 public class InheritanceTester {
+
     public static void main(String[] args){
-                Employee employee = new Employee();
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.print();
+
+        Square square = new Square();
+        square.print("Square");
+
     }
 }
 /**
